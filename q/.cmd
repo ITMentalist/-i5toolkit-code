8c9a5c9d5d2a247b376078a43f4c781834ccfaa4
junleili-cn <junleili-cn@c4e1c60a-6367-4692-a381-dd07b32abc8d>
2009-02-28 14:55:37 +0000

    initial import

---
+++ q/.cmd
@@ -0,0 +1,4 @@
+open i810
+cd /home/ljl/chqs/qtool
+put qattr.mi -o qattr.mi
+by
---
+++ q/dspqd.cmd
@@ -0,0 +1,18 @@
+/** @file qattr.cmd */
+
+             CMD        PROMPT('Create MI Program')
+
+/* program name */
+             PARM       KWD(Q) TYPE(Q_NAME) MIN(1) KEYPARM(*YES) +
+                          PROMPT('Queue object')
+             PARM       KWD(QTYPE) TYPE(*NAME) LEN(10) RSTD(*YES) +
+                          DFT(*DTAQ) SPCVAL((*DTAQ) (*USRQ)) MIN(0) +
+                          PROMPT('Queue type')
+
+/* qualifiers */
+ Q_NAME:     QUAL       TYPE(*NAME) LEN(10) MIN(1)
+             QUAL       TYPE(*NAME) LEN(10) DFT(*LIBL) +
+                          SPCVAL((*CURLIB) (*LIBL)) PROMPT('Library +
+                          name')
+
+/* EOF */
