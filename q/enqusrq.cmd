7b84ae9a392cac405822b04593570b94b68cf307
junleili-cn <junleili-cn@c4e1c60a-6367-4692-a381-dd07b32abc8d>
2009-07-08 08:30:50 +0000

    enqusrq.mi, before using SETDP on SPCPTR input-key, i should ckeck input-key-length first

--- q/enqusrq.cmd
+++ q/enqusrq.cmd
@@ -14,22 +14,26 @@
 /*  */
 /* You should have received a copy of the GNU General Public License */
 /* along with i5/OS Programmer's Toolkit.  If not, see <http://www.gnu.org/licenses/>. */
+
 /* file enqusrq.mi */
              CMD        PROMPT('Clear User Queue (CLRUSRQ)')
 
              PARM       KWD(Q) TYPE(Q_OBJ) MIN(1) PROMPT('Queue +
                           object') /* Parm1: queue object */
-             PARM       KWD(KEY) TYPE(*CHAR) LEN(256) VARY(*YES +
+             PARM       KWD(KEY) TYPE(*CHAR) LEN(256) DFT(*NONE) +
+                          SPCVAL((*NONE '')) MIN(0) VARY(*YES +
                           *INT2) CASE(*MIXED) INLPMTLEN(50) +
                           PROMPT('Key data') /* Parm2: key data */
-             PARM       KWD(MSG) TYPE(*CHAR) LEN(5000) VARY(*YES +
-                          *INT2) CASE(*MIXED) INLPMTLEN(80) +
+             PARM       KWD(MSG) TYPE(*CHAR) LEN(5000) DFT(*NONE) +
+                          SPCVAL((*NONE '')) VARY(*YES *INT2) +
+                          CASE(*MIXED) INLPMTLEN(80) +
                           PROMPT('Message text') /* Parm3: message +
                           text */
-             PARM       KWD(MSGLEN) TYPE(*DEC) LEN(5 0) +
-                          PROMPT('Message Text Length')
+             PARM       KWD(MSGLEN) TYPE(*DEC) LEN(5 0) DFT(0) +
+                          PROMPT('Message text length')
 
  Q_OBJ:      QUAL       TYPE(*NAME) LEN(10) MIN(1)
              QUAL       TYPE(*NAME) LEN(10) DFT(*LIBL) +
                           SPCVAL((*CURLIB) (*LIBL)) PROMPT('Library +
                           name')
+
