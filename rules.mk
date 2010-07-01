f7c0110cddf1a51c08d3d1d74b96543819a4e224
junleili-cn <junleili-cn@c4e1c60a-6367-4692-a381-dd07b32abc8d>
2010-07-01 11:10:28 +0000

    review before release 0.2.14

--- rules.mk
+++ rules.mk
@@ -14,10 +14,10 @@ MAKE=make
 	system "crtbndcpp $(BIN_LIB)/$@ srcstmf('$<') dbgview(*all) output(*print)"
 	ln -fs /qsys.lib/$(BIN_LIB).lib/$@.pgm $@
 .mi:
-	system "i5toolkit/mic $(BIN_LIB)/$@ srcpath($<) option(*replace *list *xref *atr)"
+	system "i5toolkit/mic $(BIN_LIB)/$@ srcpath($<) option(*replace *list *xref *atr) $(MIFLAGS)"
 	ln -fs /qsys.lib/$(BIN_LIB).lib/$@.pgm $@
 .emi:
-	system "i5toolkit/mic $(BIN_LIB)/$@ srcpath($<) option(*replace *list *xref *atr)"
+	system "i5toolkit/mic $(BIN_LIB)/$@ srcpath($<) option(*replace *list *xref *atr) $(MIFLAGS)"
 	ln -fs /qsys.lib/$(BIN_LIB).lib/$@.pgm $@
 .rpgle:
 	system "crtbndrpg $(BIN_LIB)/$@ srcstmf('$<') $(RPGLEFLAGS)"
